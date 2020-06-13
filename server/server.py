c0fa405417a7572a5a31473892b88e1837b026c5
Priscilla <sister.priscilla@gmail.com>
2020-06-13 18:22:50 -0400

    Connected to firebase

--- server/server.py
+++ server/server.py
@@ -1,6 +1,20 @@
+import pyrebase
 from flask import Flask, request
 from flask_restful import Resource, Api
 
+config = {
+    "apiKey": "AIzaSyAgLJEAk3vGgql5jsDpZi-aQgOsd03Fto0",
+    "authDomain": "munchkin-55db1.firebaseapp.com",
+    "databaseURL": "https://munchkin-55db1.firebaseio.com",
+    "projectId": "munchkin-55db1",
+    "storageBucket": "munchkin-55db1.appspot.com",
+    "messagingSenderId": "118394564063",
+    "appId": "1:118394564063:web:2b0fc5c316bb726815286a",
+    "measurementId": "G-XVKT8244CK"
+}
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+db = firebase.database()
 app = Flask(__name__)
 api = Api(app)
 
@@ -9,6 +23,11 @@ class Players(Resource):
         pass
 
 api.add_resource(Players, '/players')
+
+@app.route('/', methods=['GET','POST'])
+
+def basic():
+    return 'Welcome!'
 
 if __name__ == '__main__':
     app.run(port=5002)
